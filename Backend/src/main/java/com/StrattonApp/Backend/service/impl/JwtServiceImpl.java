303999d8958111f1753a74e21ae833e51cff5ea4
alvrichh <alvarorodriguezmolina0@gmail.com>
2024-06-12 15:33:26 +0200

    Jwt role extract from token

--- Backend/src/main/java/com/StrattonApp/Backend/service/impl/JwtServiceImpl.java
+++ Backend/src/main/java/com/StrattonApp/Backend/service/impl/JwtServiceImpl.java
@@ -10,8 +10,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import com.StrattonApp.Backend.entities.Empleado;
 import com.StrattonApp.Backend.service.JwtService;
+import com.StrattonApp.Backend.entities.Empleado;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
@@ -19,60 +19,39 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
-/**
- * Implementación del servicio JwtService para manejar tokens JWT.
- */
 @Service
 public class JwtServiceImpl implements JwtService {
 
-    // Llave para firmar el JWT, obtenida del archivo de propiedades de la aplicación.
     @Value("${jwt.secret}")
     private String jwtSigningKey;
 
-    /**
-     * Extrae el nombre de usuario del token JWT.
-     *
-     * @param token Token JWT.
-     * @return Nombre de usuario extraído del token.
-     */
     @Override
     public String extractUserName(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
-    /**
-     * Genera un token JWT para el usuario proporcionado.
-     *
-     * @param userDetails Detalles del usuario.
-     * @return Token JWT generado.
-     */
-    @Override
-    public String generateToken(UserDetails userDetails) {
-        return generateToken(new HashMap<>(), userDetails);
+    public String extractUserRole(String token) {
+        return extractClaim(token, claims -> claims.get("role", String.class));
     }
 
-    /**
-     * Verifica si el token JWT es válido para el usuario proporcionado.
-     *
-     * @param token       Token JWT.
-     * @param userDetails Detalles del usuario.
-     * @return `true` si el token es válido, `false` si no lo es.
-     */
+    @Override
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> extraClaims = new HashMap<>();
+        extraClaims.put("role", ((Empleado) userDetails).getMainRole()); // Añadir el rol a los claims
+        return generateToken(extraClaims, userDetails);
+    }
+
     @Override
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String userName = extractUserName(token);
         return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
 
-    // Método genérico para extraer información del token JWT.
-    // Los claims son declaraciones que contienen información sobre el usuario y metadatos adicionales.
-    private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers) {
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
-        return claimsResolvers.apply(claims);
+        return claimsResolver.apply(claims);
     }
 
-    // Genera el token JWT incluyendo los claims adicionales y los detalles del usuario.
-    // Los claims adicionales pueden ser usados para almacenar información adicional sobre el usuario o el token.
     private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         return Jwts.builder()
                 .setClaims(extraClaims)
@@ -83,18 +62,14 @@ public class JwtServiceImpl implements JwtService {
                 .compact();
     }
 
-    // Verifica si el token ha expirado.
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
 
-    // Extrae la fecha de expiración del token.
     private Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
-    // Extrae todas las claims del token.
-    // Aquí se extrae y se procesa el conjunto completo de claims del JWT.
     private Claims extractAllClaims(String token) {
         return Jwts.parser()
                 .setSigningKey(getSigningKey())
@@ -103,7 +78,6 @@ public class JwtServiceImpl implements JwtService {
                 .getBody();
     }
 
-    // Obtiene la llave de firma a partir de la cadena codificada en base64.
     private Key getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode(jwtSigningKey);
         return Keys.hmacShaKeyFor(keyBytes);
--- Backend/src/main/java/com/StrattonApp/Backend/utils/TokenUtils.java
+++ Backend/src/main/java/com/StrattonApp/Backend/utils/TokenUtils.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import com.StrattonApp.Backend.entities.Role;
 import com.StrattonApp.Backend.exceptions.GlobalException;
 
 import io.jsonwebtoken.Claims;
@@ -21,120 +22,115 @@ import io.jsonwebtoken.security.Keys;
 
 
 public class TokenUtils {
-	
-	
-	
-	// Declaramos el token que usaremos para el cifrado y la decodificación.
-		// Lo suyo es que este token sea generado de forma aleatoria al empezar la
-		// aplicación
-		// Podemos poner el valor que queramos
-		private final static String ACCESS_TOKEN_SECRET = "586E3272357538782F413F4428472B4B6250655368566B597033733676397924";
+    
+    // Declaramos el token que usaremos para el cifrado y la decodificación.
+    // Lo suyo es que este token sea generado de forma aleatoria al empezar la
+    // aplicación
+    // Podemos poner el valor que queramos
+    private final static String ACCESS_TOKEN_SECRET = "586E3272357538782F413F4428472B4B6250655368566B597033733676397924";
 
-		// Definimos el tiempo de validez del token en segundos. Mientras estemos en
-		// fase de desarrollo
-		// podemos definir tiempos largos para que no nos den problema la caducidad del
-		// token.
-		// Cuando nuestra aplicación esté en explotación deberemos ajustar este tiempo.
-		// Estos dos parámetro también se pueden definir el el fichero
-		// aplicattion.properties
+    // Definimos el tiempo de validez del token en segundos. Mientras estemos en
+    // fase de desarrollo
+    // podemos definir tiempos largos para que no nos den problema la caducidad del
+    // token.
+    // Cuando nuestra aplicación esté en explotación deberemos ajustar este tiempo.
+    // Estos dos parámetro también se pueden definir el el fichero
+    // aplicattion.properties
 
-		private final static Long ACCESS_TOKEN_VALIDATY_SECONDS = (long) 3 * 60 * 60; // 3 horas
+    private final static Long ACCESS_TOKEN_VALIDATY_SECONDS = (long) 3 * 60 * 60; // 3 horas
 
-		/**
-		 * Este método va a generar un token. En el token incluiremos el username, 
-		 *  role. Esto es un ejemplo, en el token podemos almacenar la información que
-		 * necesite nuestra aplicación, por ejemplo, nombre y apellido o lo que
-		 * consideremos necesario.
-		 * 
-		 * @param username: guardaremos el email dentro del token
-		 * @param role:     guardaremos el role dentro del token. Si puediera tener
-		 *                  varios roles, también se podría guardar.
-		 * @return: el token que debe empezar por Bearer y un espacio.
-		 */
-		
-		
-		public static String generateToken(String username, String name, String rol, Integer id) {
+    /**
+     * Este método va a generar un token. En el token incluiremos el username,
+     * role. Esto es un ejemplo, en el token podemos almacenar la información que
+     * necesite nuestra aplicación, por ejemplo, nombre y apellido o lo que
+     * consideremos necesario.
+     * 
+     * @param username: guardaremos el email dentro del token
+     * @param name:     nombre del usuario
+     * @param rol:      role del usuario
+     * @param id:       ID del usuario
+     * @return el token que debe empezar por Bearer y un espacio.
+     */
+    public static String generateToken(String username, String name, Role rol, Integer id) {
 
-			// Establecemos la fecha de expiración del token en milisegundos
-			Date expirationDate = new Date(System.currentTimeMillis() + ACCESS_TOKEN_VALIDATY_SECONDS * 1000);
+        // Establecemos la fecha de expiración del token en milisegundos
+        Date expirationDate = new Date(System.currentTimeMillis() + ACCESS_TOKEN_VALIDATY_SECONDS * 1000);
 
-			// Creamos un mapa para guardar toda la información que queramos guardar en el
-			// token
-			// El username no es necesario guardarlo porque ya va en el token, en el
-			// subject.
-			Map<String, Object> extra = new HashMap<>();
-			extra.put("name", name);
-			extra.put("rol", rol);
-			extra.put("id", id);
-			
+        // Creamos un mapa para guardar toda la información que queramos guardar en el
+        // token
+        // El username no es necesario guardarlo porque ya va en el token, en el
+        // subject.
+        Map<String, Object> extra = new HashMap<>();
+        extra.put("name", name);
+        extra.put("rol", rol.name()); // Guardamos el nombre del rol
+        extra.put("id", id);
 
-			// Construimos el token con el nombre del usuario, la fecha de expiración, la
-			// información
-			// que queremos guardar y el token que vamos a user para encriptarlo.
-			String token = Jwts.builder().subject(username).issuedAt(new Date())
-					.expiration(expirationDate).claims(extra)
-					.signWith(Keys.hmacShaKeyFor(ACCESS_TOKEN_SECRET.getBytes())).compact();
+        // Construimos el token con el nombre del usuario, la fecha de expiración, la
+        // información
+        // que queremos guardar y el token que vamos a user para encriptarlo.
+        String token = Jwts.builder().subject(username).issuedAt(new Date())
+                .expiration(expirationDate).claims(extra)
+                .signWith(Keys.hmacShaKeyFor(ACCESS_TOKEN_SECRET.getBytes())).compact();
 
-			return "Bearer " + token;
-		}
+        return "Bearer " + token;
+    }
 
-		/**
-		 * Obtiene el payLoad de un token
-		 * 
-		 * @param token
-		 * @return
-		 * @throws JwtException
-		 * @throws IllegalArgumentException
-		 * @throws NoSuchAlgorithmException
-		 */
-		public static Claims getAllClaimsFromToken(String token)
-				throws JwtException, IllegalArgumentException, NoSuchAlgorithmException {
+    /**
+     * Obtiene el payLoad de un token
+     * 
+     * @param token
+     * @return
+     * @throws JwtException
+     * @throws IllegalArgumentException
+     * @throws NoSuchAlgorithmException
+     */
+    public static Claims getAllClaimsFromToken(String token)
+            throws JwtException, IllegalArgumentException, NoSuchAlgorithmException {
 
-			Claims claims = Jwts.parser().verifyWith(Keys.hmacShaKeyFor(ACCESS_TOKEN_SECRET.getBytes())).build()
-					.parseSignedClaims(token).getPayload();
-			
-			return claims;
-		}
+        Claims claims = Jwts.parser().verifyWith(Keys.hmacShaKeyFor(ACCESS_TOKEN_SECRET.getBytes())).build()
+                .parseSignedClaims(token).getPayload();
+        
+        return claims;
+    }
 
-		/**
-		 * Método para ver el usuario y el role que "contiene" el token. Lo primero que
-		 * haremos es decodificar el claims. Si lanza una exception es que no es válido
-		 * usando nuestra token secreto. Si es válido vemos la fecha de expiracion
-		 * Sacamos el nombre y los roles y lo devolvemos.
-		 * 
-		 * @param token
-		 * @return
-		 * 
-		 */
-		public static UsernamePasswordAuthenticationToken getAuthentication(String token)
-				throws JwtException, IllegalArgumentException, NoSuchAlgorithmException {
-			Claims claims;
+    /**
+     * Método para ver el usuario y el role que "contiene" el token. Lo primero que
+     * haremos es decodificar el claims. Si lanza una exception es que no es válido
+     * usando nuestra token secreto. Si es válido vemos la fecha de expiracion
+     * Sacamos el nombre y los roles y lo devolvemos.
+     * 
+     * @param token
+     * @return
+     * 
+     */
+    public static UsernamePasswordAuthenticationToken getAuthentication(String token)
+            throws JwtException, IllegalArgumentException, NoSuchAlgorithmException {
+        Claims claims;
 
-			if (!token.startsWith("Bearer ")) {
-				throw new GlobalException("Formato token no válido");
-			}
-			token = token.substring(7);
-			try {
-				// Claims == PayLoad
-				claims = getAllClaimsFromToken(token);
-			} catch (IllegalArgumentException e) {
-				throw new GlobalException("Imposible encontra un JWT Token");
-			} catch (ExpiredJwtException e) {
-				throw new GlobalException("Token expirado");
-			} catch (NoSuchAlgorithmException e) {
-				throw new GlobalException("Algoritmo no válido");
-			}catch (MalformedJwtException e) {
-				throw new GlobalException("Token malformado");
-			}
+        if (!token.startsWith("Bearer ")) {
+            throw new GlobalException("Formato token no válido");
+        }
+        token = token.substring(7);
+        try {
+            // Claims == PayLoad
+            claims = getAllClaimsFromToken(token);
+        } catch (IllegalArgumentException e) {
+            throw new GlobalException("Imposible encontra un JWT Token");
+        } catch (ExpiredJwtException e) {
+            throw new GlobalException("Token expirado");
+        } catch (NoSuchAlgorithmException e) {
+            throw new GlobalException("Algoritmo no válido");
+        }catch (MalformedJwtException e) {
+            throw new GlobalException("Token malformado");
+        }
 
-			
-			String username = claims.getSubject();
-			String rol = (String) claims.get("rol");
-			List<SimpleGrantedAuthority> authorities = new ArrayList<>();
-			authorities.add(new SimpleGrantedAuthority(rol));
+        String username = claims.getSubject();
+        String rol = (String) claims.get("rol");
+        Role roleEnum = Role.valueOf(rol); // Convertimos el string del rol a un enum Role
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(roleEnum.name()));
 
-			return new UsernamePasswordAuthenticationToken(username, null, authorities);
+        return new UsernamePasswordAuthenticationToken(username, null, authorities);
 
-		}
-
+    }
 }
